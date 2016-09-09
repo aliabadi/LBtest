55d3397ead8eaad1890498a9d4f494533d23c08e
Nicholas <aliabadi@uw.edu>
2016-09-09 08:21:58 -0700

    datasources

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,9 +1,10 @@
-var mongoUri = process.env.MONGODB_URI || 
+var mongoUri = process.env.MONGODB_URI ||
   process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL;
 
 module.exports = {
   mongodb: {
+    name: "mongoDs",
     defaultForType: "mongodb",
     connector: "loopback-connector-mongodb",
     url: mongoUri
